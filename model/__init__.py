ea2ba88ce680847de7483d079cb70b20dc0ae0b2
Beer thuis <beer.pruijt@hotmail.com>
2023-12-14 14:22:34 +0100

    deleted model.gru from __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,2 @@
 from .h_gru import train_hierarchical_gru, generate_forecasts
-from .gru import define_gru_model
 from .torch_gru import GRUNet, create_sequence_target_pairs, load_data, log_diff, initialize_model, train_model, convert_forecasts_to_series, forecast_using_gru
